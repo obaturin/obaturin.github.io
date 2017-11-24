cf82c8b5f2d277ca2b1463e3678386027514b4e9
obaturin <32974956+obaturin@users.noreply.github.com>
2017-11-25 00:35:18 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,115 +1,124 @@
 $(function() {
-
-    const display = $('.maincontent');
-    const sections = $('.section');
-
-    let inScroll = false;
-
-    const mobileDetect = new mobileDetect(window.navigator.userAgent);
-    const isMobile = mobileDetect.mobile();
-
-    const switchMenuActiveClass = sectionEq => {
-        $('.vertical-nav__item').eq(sectionEq).addClass('vertical-nav__item_active')
-            .siblings().removeClass('vertical-nav__item_active');
-    }
-
-    const performTransition = sectionEq => {
-        if (inScroll == true) return 
-        inScroll = true
-
-        const position = (sectionEq * -100) + '%';
     
-        display.css({
-            'transform' : `translate(0, ${position})`,
-            '-webkit-transform' : `translate(0, ${position})`
-        })
-
-        sections.eq(sectionEq).addClass('active')
-            .siblings().removeClass('active');
-        
-        setTimeout(() => {
-            inScroll = false;
-            switchMenuActiveClass(sectionEq);
-        }, 1300);
-    }
-
-
-    const difineSections = sections => {
-        const activeSection = sections.filter('.active');
-        return {
-            activeSection: activeSection,
-            nextSection: activeSection.next(),
-            prevSection: activeSection.prev()
+        const display = $('.maincontent');
+        const sections = $('.section');
+    
+        let inScroll = false;
+    
+        const mobileDetect = new MobileDetect(window.navigator.userAgent);
+        const isMobile = mobileDetect.mobile();
+    
+        const switchMenuActiveClass = sectionEq => {
+            $('.vertical-nav__item').eq(sectionEq).addClass('vertical-nav__item_active')
+                .siblings().removeClass('vertical-nav__item_active');
         }
-    }
-
-
-    $('.wrapper').on({
-        wheel : e => {
-            const deltaY = e.originalEvent.deltaY;
-            const section = difineSections(sections);
-
-            if (deltaY > 0 && section.nextSection.length) {
+    
+        const performTransition = sectionEq => {
+            if (inScroll == true) return 
+            inScroll = true
+    
+            const position = (sectionEq * -100) + '%';
+        
+            display.css({
+                'transform' : `translate(0, ${position})`,
+                '-webkit-transform' : `translate(0, ${position})`
+            })
+    
+            sections.eq(sectionEq).addClass('active')
+                .siblings().removeClass('active');
+            
+            setTimeout(() => {
+                inScroll = false;
+                switchMenuActiveClass(sectionEq);
+            }, 1300);
+        }
+    
+        const difineSections = sections => {
+            const activeSection = sections.filter('.active');
+            return {
+                activeSection: activeSection,
+                nextSection: activeSection.next(),
+                prevSection: activeSection.prev()
+            }
+        }
+    
+        const scrollToSection = direction => {
+            const section = difineSections(sections)
+    
+            if (inScroll) return;
+    
+            if (direction === 'up' && section.nextSection.length) { // вниз
                 performTransition(section.nextSection.index())
             }
-
-            if (deltaY <0 && section.prevSection.length) {
+    
+            if (direction === 'down' && section.prevSection.length) { // вверх
                 performTransition(section.prevSection.index())
             }
-        },
-        touchmove: e => (e.preventDefault())
-    });
-
-
-    $(document).on('keydown', e => {
-        const section = difineSections(sections);
-
-        if (inScroll) return
-
-        switch (e.keyCode) {
-            case 40:
-                if (!section.nextSection.length) return;
-                performTransition(section.nextSection.index());
-                break;
-
-            case 38:
-                if (!section.prevSection.length) return;
-                performTransition(section.prevSection.index());
-                break;
         }
-    });
-
-    if (isMobile) {
-        $(window).swipe({
-            swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
+    
+        $('.wrapper').on({
+            wheel: e => {
+              const deltaY = e.originalEvent.deltaY;
+              let direction = (deltaY > 0) 
+                ? 'up' 
+                : 'down'
+          
+              scrollToSection(direction);
+            },
+            touchmove: e => (e.preventDefault())
+          });
+    
+        $(document).on('keydown', e => {
+            const section = difineSections(sections);
+    
+            if (inScroll) return
+    
+            switch (e.keyCode) {
+                case 40:
+                    if (!section.nextSection.length) return;
+                    performTransition(section.nextSection.index());
+                    break;
+    
+                case 38:
+                    if (!section.prevSection.length) return;
+                    performTransition(section.prevSection.index());
+                    break;
             }
-        })
-    }
+        });
+    
+        if (isMobile) {
+            $(window).swipe({
+              swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+                // console.log(direction);
+                scrollToSection(direction);
+              }
+            })
+          }
+        
+    
+        $('[data-scroll-to]').on('click touchstart', e => {
+            e.preventDefault();
+            const $this = $(e.currentTarget);
+            const sectionIndex = parseInt($this.attr('data-scroll-to'));
+    
+            performTransition(sectionIndex);
+        });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    })
     
 
-    $('[data-scroll-to]').on('click touchstart', e => {
-        e.preventDefault();
-        const $this = $(e.currentTarget);
-        const sectionIndex = parseInt($this.attr('data-scroll-to'));
-
-        performTransition(sectionIndex);
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
 
 
 
